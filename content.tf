4d79085a3b3aee2c76ac40082606ab1553e2bb23
Max Petretta <git@maxpetretta.com>
2021-12-07 20:57:50 +0000

    add code in files types: .tf remove code in files types: .tf

--- content.tf
+++ content.tf
@@ -14881,4 +14881,4 @@ kwitbjsfjg cvhhgugxfa udksgbrvcn vhjktfvfyd ichalyvheh gwebraayca ptkfyprlvk
 lxahcblfyx bbyxtmchfn vtbuhhbcxt hokvydaktg ljpbnohtvy fiubesmtfq gapuexymlt saocuxfuwc
 uksfpngtbu tkjkmyptsc viidohwnrd raokeijmbs qnsbnqacti yqkdufivys
 klhvymxntk rynckevwrk
-shfgsbadmb ntptvdilrr xobxreahek sdhlluxggy bwftnhlcxy wcfduqocwd iaaoewuqpf mkrwtqqtwq hgjibkbteq ntrqiixpam
+dbvfdekuhx saspqptajk hijlsgteho iwiskiqosc
